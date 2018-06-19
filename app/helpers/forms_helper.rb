10d5600de446a456b03d637e585f61bd585b6848
mharribey <mathieuharribey@hotmail.fr>
2018-06-19 19:06:41 +0200

    did qcm for multiple answers

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -1,3 +1,15 @@
 module FormsHelper
 
+  def matchAnswers(user, form)
+    x = 0
+    form.questions.each do |question|
+      q = question.answers.where(validation: true)
+      u = user.answers.where(question_id: question)
+      if q == u
+        x += 1
+      end
+    end
+    return x
+  end
+
 end
